19d4113a7df3820753583771b8aa6ea821c54477
Rimantas <belauzas@yahoo.com>
2021-05-25 18:50:35 +0300

    Tuck

--- tuck.js
+++ tuck.js
@@ -5,3 +5,16 @@ Arejuje nurodyta, kiek sekundziu reikia laukti, kol bus isspausdinta nurodyta re
 
 const a = [2, 3, 1, 4, 2];
 
+function waiting(list) {
+    const time = list[0];
+    setTimeout(() => {
+        console.log(time);
+        list.shift()
+
+        if (list.length > 0) {
+            waiting(list);
+        }
+    }, time * 1000)
+}
+
+waiting(a);
